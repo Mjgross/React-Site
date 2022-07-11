97fef01bb89e3fea3c91703eb78d7ec28e98220a
Matt <mjgross1@asu.edu>
2022-07-10 19:56:44 -0700

    update constants file

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,6 +1,6 @@
 export const AUTH_TOKEN = 'auth-token';
 export const MAX_PAGE_SIZE = '1080px';
-export const PROJECT_NAME = "React-Site-Project";
+export const PROJECT_NAME = "React-Site";
 
 export function baseUrl() {
     var base_url = "/";
